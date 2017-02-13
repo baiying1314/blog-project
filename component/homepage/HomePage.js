19d891995ef61a3a19f6bbdca17ae7ce117e0a37
code <1165444511@qq.com>
2017-02-13 15:41:58 +0800

    Refinement components in homepage

--- component/homepage/HomePage.js
+++ component/homepage/HomePage.js
@@ -4,8 +4,35 @@ import {Link} from 'react-router'
 class HomePage extends React.Component {
     render() {
         return <div>
+            <Navigation />
+            <Title />
+            <Show />
             <h1>I am homePage</h1>
           <Link to = "personpage">个人主页</Link>
+        </div>
+    }
+}
+
+class Navigation extends React.Component{
+    render(){
+        return <div>
+            I am Navigation
+        </div>
+    }
+}
+
+class Title extends React.Component{
+    render(){
+        return <div>
+            I am title
+        </div>
+    }
+}
+
+class Show extends React.Component{
+    render(){
+        return <div>
+            I am show
         </div>
     }
 }
